101f800435d56c82737e19508ea6af8d3a8b2f67
Natalia Pattarone <nataliapattarone@gmail.com>
2017-02-16 17:41:43 -0300

    extra info in console

--- app/scripts/controllers/signalr.js
+++ app/scripts/controllers/signalr.js
@@ -11,13 +11,15 @@ angular.module('magicmirrorApp')
 .controller('signalRController', ['$scope', 'backendHubProxy',
   function ($scope, backendHubProxy) {
     console.log('trying to connect to service');
-    var readerDataHub = backendHubProxy(backendHubProxy.defaultServer, 'readerHub');
+    var readerDataHub = backendHubProxy(backendHubProxy.defaultServer, 'ReaderHub');
 
     console.log('connected to service');
     $scope.readerProductDescription = "";
     $scope.readerProductCode = "";
 
     readerDataHub.on('broadcast', function (data) {
+      console.log('broadcast: ' + data);
+
       data.forEach(function (readerInfo, $scope) {
             $scope.readerProductDescription = readerInfo.description;
             $scope.readerProductCode = readerInfo.code;
